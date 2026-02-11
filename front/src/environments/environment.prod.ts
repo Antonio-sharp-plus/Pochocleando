8a0279aab6495139933073e8c126116bd34ca8bd
Antonio-sharp-plus <antonicoa2014@gmail.com>
2026-02-11 20:55:14 -0300

    Ajustes para hosteo en Cloudfare

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // acá seteo que cuando buildee la app para amplify, va a usar esta apiURL que me dio ApiGateway en AWS
 
 export const environment = {
-    production: true,
-    apiUrl: 'https://pkuq7prfnd.execute-api.us-east-1.amazonaws.com/api'
-}
+  production: true,
+  apiUrl: 'proyecto-pochocleando-production.up.railway.app',
+};
